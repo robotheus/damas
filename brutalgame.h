514e60e83fef3d7eb1835a219040ecc23bcb2399
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2022-09-26 23:25:23 -0300

    Delete brutalgame.h

--- brutalgame.h
+++
@@ -1,6 +0,0 @@
-void brutal_game(int **, int, int);
-int check_diagonals(int **, int, int, int, int, int, int, int *, int, int **, int, int, int *, int *, int, int);
-void eat(int **, int, int, int, int, int, int, int *, int, int **, int , int, int *, int *, int, int);
-int nmb_diagonals(int **, int, int, int, int);
-int count_flags(int **, int **, int, int, int, int, int, int, int, int, int*, int*, int, int, int);
-void clean_flags(int **, int, int, int);
